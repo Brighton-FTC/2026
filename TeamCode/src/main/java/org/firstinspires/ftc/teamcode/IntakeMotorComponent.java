a9f9fa0041d659e4c4bfe0c0a8c18cf657d64ab4
Phyeory <jamjamfr@proton.me>
2025-10-01 16:04:57 +0100

    Stop motor procedure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeMotorComponent.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class IntakeMotorComponent {
+
+    private final Motor motor;
+    public IntakeMotorComponent(HardwareMap hardwareMap, String motorID){
+        motor = new Motor(hardwareMap, motorID);
+
+    }
+
+    public void startMotor(){
+        motor.set(1);
+    }
+
+    public void stopMotor(){
+        motor.stopMotor();
+    }
+
+}
