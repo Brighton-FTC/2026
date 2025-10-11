835e0f33da9ba24a64e4a6c345380cd3535cbb7a
Phyeory <jamjamfr@proton.me>
2025-10-11 14:23:52 +0100

    Tested april tag localisation and motif recognition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagTester.java
@@ -5,6 +5,7 @@ import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
@@ -18,6 +19,8 @@ public class AprilTagTester extends OpMode {
 
     private boolean webcamOn = false;
 
+    private int state;
+
     private AprilTagLocalization camera;
     private Position cameraPosition = new Position(DistanceUnit.INCH,
             0, 0, 0, 0);
@@ -26,29 +29,42 @@ public class AprilTagTester extends OpMode {
 
     @Override
     public void init(){
-        camera = new AprilTagLocalization(hardwareMap, cameraPosition, cameraOrientation, "Webcam 1");
+        camera = new AprilTagLocalization(hardwareMap, cameraPosition, cameraOrientation, "Webcam 1", telemetry);
 
         gamepad = new GamepadEx(gamepad1);
+
+        camera.stopStreaming();
 
     }
 
     @Override
     public void loop() {
+        gamepad.readButtons();
+
         if (gamepad.wasJustPressed(PSButtons.SQUARE) && !webcamOn){
-            camera.startStreaming();
-            camera.telemetryAprilTag();
+            state = 1;
+            webcamOn = true;
         } else if (gamepad.wasJustPressed(PSButtons.SQUARE) && webcamOn) {
             camera.stopStreaming();
             webcamOn = false;
         }
 
         if (gamepad.wasJustPressed(PSButtons.CIRCLE) && !webcamOn){
-            camera.startStreaming();
-            camera.checkCase();
+            state = 2;
+            webcamOn = true;
         } else if (gamepad.wasJustPressed(PSButtons.CIRCLE) && webcamOn) {
+            state = 3;
             camera.stopStreaming();
             webcamOn = false;
         }
+        if (state == 1){
+            camera.startStreaming();
+            camera.telemetryAprilTag();
+        } else if (state == 2) {
+            camera.startStreaming();
+            camera.checkCase();
+        }
+        telemetry.update();
     }
 
 }
