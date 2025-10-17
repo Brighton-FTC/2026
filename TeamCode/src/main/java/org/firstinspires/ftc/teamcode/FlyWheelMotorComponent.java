1e56b57d84de51856f775a6be92cf307228095d1
Phyeory <jamjamfr@proton.me>
2025-10-17 20:01:24 +0100

    Added PID for fly wheel motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlyWheelMotorComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlyWheelMotorComponent.java
@@ -1,15 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+@Config
+@Configurable
 public class FlyWheelMotorComponent {
 
     private final Motor motor;
 
+    public double kP;
+    public double kI;
+    public double kD;
+    public double kS;
+    public double kV;
+    public double kA;
+
+
+
     public FlyWheelMotorComponent(HardwareMap hardwareMap, String motorID){
         motor = new Motor(hardwareMap, motorID);
         motor.setRunMode(Motor.RunMode.VelocityControl);
+        motor.setVeloCoefficients(kP, kI, kD);
+        motor.setFeedforwardCoefficients(kS, kV, kA);
+
     }
 
     public void runMotorAt(double velocity){
