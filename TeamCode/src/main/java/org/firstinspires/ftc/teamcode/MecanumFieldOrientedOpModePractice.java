49a2ba431d89929e8e0c5150d9f85ba257138669
bouncy2019 <144822349+bouncy2019@users.noreply.github.com>
2025-11-20 20:36:16 +0000

    Minor changes to mecanum drive
    Makes controlling robot easier

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFieldOrientedOpModePractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFieldOrientedOpModePractice.java
@@ -4,6 +4,7 @@ import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrivePractice;
 
@@ -25,8 +26,10 @@ public class MecanumFieldOrientedOpModePractice extends OpMode {
         forward = Math.abs(gamepad1.left_stick_y) > DEAD_ZONE_SIZE ? gamepad1.left_stick_y : 0;
         strafe = Math.abs(gamepad1.left_stick_x) > DEAD_ZONE_SIZE ? gamepad1.left_stick_x : 0;
         rotate = Math.abs(gamepad1.right_stick_x) > DEAD_ZONE_SIZE ? gamepad1.right_stick_x : 0;
+        forward = (Math.pow(forward, 3) + forward) / 2;
+        strafe = (Math.pow(strafe, 3) + strafe) / 2;
+        rotate = (Math.pow(rotate, 3) + rotate) / 2;
 
         drive.driveFieldRelative(forward,strafe,rotate);
-
     }
 }
