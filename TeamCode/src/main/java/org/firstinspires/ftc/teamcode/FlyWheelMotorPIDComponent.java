1b0e20f1b9391ecc527fa18d39db62b1993f1630
Phyeory <jamjamfr@proton.me>
2025-12-14 22:08:05 +0000

    edited flywheel tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlyWheelMotorPIDComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlyWheelMotorPIDComponent.java
@@ -1,36 +1,45 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.wpilibcontroller.SimpleMotorFeedforward;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @Config
 @Configurable
-public class FlyWheelMotorComponent {
+public class FlyWheelMotorPIDComponent {
 
     private final Motor motor;
 
-    public double kP;
-    public double kI;
-    public double kD;
+    public double kP = 0.0015;
+    public double kI = 0 ;
+    public double kD = 0;
     public double kS;
     public double kV;
-    public double kA;
+    private SimpleMotorFeedforward ff = new SimpleMotorFeedforward(kS, kV);
+
+    private PIDController controller = new PIDController(kP, kI, kD);
 
 
 
-    public FlyWheelMotorComponent(HardwareMap hardwareMap, String motorID){
+    public FlyWheelMotorPIDComponent(HardwareMap hardwareMap, String motorID){
         motor = new Motor(hardwareMap, motorID);
-        motor.setRunMode(Motor.RunMode.VelocityControl);
-        motor.setVeloCoefficients(kP, kI, kD);
-        motor.setFeedforwardCoefficients(kS, kV, kA);
-
     }
 
     public void runMotorAt(double velocity){
-        motor.set(velocity);
+        controller.setSetPoint(velocity);
+        double PIDpower = controller.calculate(motor.getCorrectedVelocity(), velocity);
+        double FFpower = ff.calculate(velocity);
 
+
+        motor.set(FFpower + PIDpower);
+
+    }
+
+    public double getVel(){
+        return motor.getCorrectedVelocity();
     }
 
     public void stopMotor(){
