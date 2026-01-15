2b9065a7c6115ff3495cf2ad85ba4d9765c5c43e
Phyeory <jamjamfr@proton.me>
2026-01-15 16:46:04 +0000

    merged with flywheels branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTester.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp
 public class ServoTester extends OpMode{
 
     //to use init and loop we have to extend OpMode.
