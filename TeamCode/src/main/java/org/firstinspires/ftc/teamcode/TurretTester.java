96e36aa9ee5b190f44815b062c20b69fdc1dadc2
Phyeory <jamjamfr@proton.me>
2026-01-23 19:56:25 +0000

    synced change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretTester.java
@@ -8,10 +8,10 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class TurretTester extends OpMode {
 
-    private TurretComponent turret;
+    private TurretPIDComponent turret;
     private GamepadEx gamepad;
 
-    private final Pose startingPose = new Pose(0, 0, Math.toRadians(0));
+    public final Pose startingPose = new Pose(12, 132, Math.toRadians(315));
 
     public boolean run = false;
 
@@ -21,17 +21,20 @@ public class TurretTester extends OpMode {
     public void init(){
         gamepad = new GamepadEx(gamepad1);
         //Scaling factor == 3.214 in degrees; 0.0561 in radians. (360*0.25/28)
-        turret = new TurretComponent(hardwareMap, "turretMotor", 0.167, -72, 72, startingPose, telemetry);
+        turret = new TurretPIDComponent(hardwareMap, "turretMotor", 0.167, 0, 144, startingPose, telemetry);
     }
 
     @Override
     public void loop(){
+        telemetry.addData("x", turret.getXPos());
+        telemetry.addData("y", turret.getYPos());
         gamepad.readButtons();
         if (gamepad.wasJustPressed(PSButtons.CIRCLE)){
             run = true;
         }
         if (run){
-            turret.aimToObject();
+            turret.aimToObject(12, 132, Math.toRadians(315));
         }
+        telemetry.update();
     }
 }
