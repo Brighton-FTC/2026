b131555ec4a7f28d79eca26a20e62c34a309d725
Phyeory <jamjamfr@proton.me>
2026-02-12 15:54:29 +0000

    fixed TurretTester merge issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turret/TurretTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turret/TurretTester.java
@@ -1,10 +1,11 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Turret;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.PSButtons;
 import org.firstinspires.ftc.teamcode.Turret.TurretPIDComponent;
 
 @TeleOp
