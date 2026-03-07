125f6302e001aaa0cf15ef402801403d0f5837ef
Kacey Au <au.kacey@gmail.com>
2026-03-07 00:47:03 +0000

    very rough tuning done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -13,7 +15,14 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants().mass(10.1);
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(10.1)
+            .forwardZeroPowerAcceleration(-446.4069556438202)
+            .lateralZeroPowerAcceleration(-784.9680929506138)
+            .translationalPIDFCoefficients(new PIDFCoefficients(1.275,0.015,0.0025,0))
+            .headingPIDFCoefficients(new PIDFCoefficients(5, 0.015, 0.0015, 0))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.7,0.015,0.0015,0.8,0))
+            .centripetalScaling(0.0005);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
@@ -27,7 +36,9 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .xVelocity(79.99456839213022)
+            .yVelocity(81.11680325632064);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
